dd3225ae6b3f561b5bf8dbaca355631d2db729d1
AndurGaitan <andresdavidagaitan@hotmail.com>
2022-11-03 10:53:07 -0300

    modificaciones

--- Desafio2/app.js
+++ Desafio2/app.js
@@ -2,13 +2,14 @@ const fs = require('fs')
 
 function generarId(info) {
     let listaNum = info.map(({ id }) => id)
-    return listaNum.includes(listaNum.length + 1) ? (Math.max(...listaNum)) : (listaNum.length + 1)
+    return listaNum.includes(listaNum.length + 1) ? (Math.max(...listaNum) + 1) : (listaNum.length + 1)
 }
 
 class Contenedor {
     constructor(archivo) {
-        this.archivo = archivo
+        this.archivo = archivo;
     }
+
     async save(producto) {//obj
         try {
             let id = await fs.promises.readFile(this.archivo, "utf-8")
@@ -19,7 +20,7 @@ class Contenedor {
                     let info = JSON.parse(data)
                     producto.id = generarId(info)
                     info.push(producto)
-                    fs.watchFile(this.archivo, JSON.stringify(info, null, 2), (err) => {
+                    fs.writeFile(this.archivo, JSON.stringify(info, null, 2), (err) => {
                         if (err) {
                             throw new Error(err);
                         }
@@ -38,7 +39,7 @@ class Contenedor {
             let item = await fs.promises.readFile(this.archivo, "utf-8")
                 .then(data => {
                     let info = JSON.parse(data)
-                    return info.find(producto.id === num)
+                    return info.find(producto => producto.id === num)
                 })
                 .then(producto => producto);
             return item
@@ -50,12 +51,16 @@ class Contenedor {
 
     async getAll() {
         try {
-            let lista = await this.getAll()
+            let lista = await fs.promises.readFile(this.archivo, "utf-8")
             //fs.promises.readFile(this.archivo, "utf-8")
                 .then(data => {
                     let info = JSON.parse(data)
                     return info
-                })
+            })
+            .then(
+                res => res
+            )
+            return lista
         }
         catch (err) {
             return []
@@ -83,11 +88,12 @@ async function ejecutar(){
 
 const c1 = new Contenedor("productos.txt");
 
-let id = await c1.save({nombre: "galletas", precio: "diez"})
+let id = await c1.save({title: "galletas", price: 200, thumbnail: "https://t1.rg.ltmcdn.com/es/posts/6/2/9/galletas_con_chispas_de_chocolate_caseras_35926_orig.jpg"})
+// await c1.save({nombre: "pan", precio: "dos"})
 
-let info = await c1.getAll()
+//let info = await c1.getAll()
 
-console.log(info)
+console.log(await c1.getAll())
 
 await c1.deleteById(id)
 
