4765e96c1db96a50c82e849f1af3ae78f50bf24c
AndurGaitan <andresdavidagaitan@hotmail.com>
2022-12-14 19:27:54 -0300

    modificaciones primera entrega

--- PreEntrega/src/server.js
+++ PreEntrega/src/server.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import path from 'path';
-//import { fileURLToPath } from 'url';
 import productsRouter from './routes/productRoutes.js';
 import cartRouter from './routes/cartRoutes.js';
-//const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080
