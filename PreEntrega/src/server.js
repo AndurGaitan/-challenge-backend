3d81333684563ad2c93484eab0874ce64598f318
AndurGaitan <andresdavidagaitan@hotmail.com>
2022-12-14 21:52:45 -0300

    md

--- PreEntrega/src/server.js
+++ PreEntrega/src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import productsRouter from './routes/productRoutes.js';
 import cartRouter from './routes/cartRoutes.js';
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8080
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
