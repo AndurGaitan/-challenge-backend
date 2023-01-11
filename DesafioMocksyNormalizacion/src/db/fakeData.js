9bbc36089a7874daa770fcdf1d042018c04cd1da
AndurGaitan <andresdavidagaitan@hotmail.com>
2023-01-11 14:00:52 -0300

    agregando desafio

---
+++ DesafioMocksyNormalizacion/src/db/fakeData.js
@@ -0,0 +1,9 @@
+
+import { faker } from '@faker-js/faker'
+faker.locale = 'es'
+
+export const fakeProds = () => ({
+    title: faker.commerce.product(),
+    price: faker.commerce.price(),
+    thumbnail: faker.image.avatar()
+});
